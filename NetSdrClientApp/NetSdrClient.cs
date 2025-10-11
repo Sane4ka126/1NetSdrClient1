04912d869dd8b939e03a3f677bf79d82ab010a7d
Sane4ka126 <evtikoleg@gmail.com>
2025-10-11 15:10:57 +0300

    Update NetSdrClient.cs
    Non-nullable field 'responseTaskSource' must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring the field as nullable.
    roslyn:CS8618 external_roslyn:CS8618roslyn

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -132,6 +132,7 @@ namespace NetSdrClientApp
         }
 
         private TaskCompletionSource<byte[]>? responseTaskSource;
+        
 
         private async Task<byte[]> SendTcpRequest(byte[] msg)
         {
