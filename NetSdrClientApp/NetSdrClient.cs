9ca8142a63d3d4be4874ed175630d9f9e8b254ed
Sane4ka126 <evtikoleg@gmail.com>
2025-10-11 15:36:31 +0300

    Update NetSdrClient.cs

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -136,7 +136,7 @@ namespace NetSdrClientApp
 
         private TaskCompletionSource<byte[]>? responseTaskSource;
 
-        private async Task<byte[]> SendTcpRequest(byte[] msg)
+        private async Task<byte[]?> SendTcpRequest(byte[] msg)
         {
             if (!_tcpClient.Connected)
             {
