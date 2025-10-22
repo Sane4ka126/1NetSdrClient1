cd801712c88c74ba69acaa993877221fe5ff05d1
Sane4ka126 <evtikoleg@gmail.com>
2025-10-22 23:32:18 +0300

    Delete EchoTspServer/Abstractions directory

--- EchoTspServer/Abstractions/ITcpListenerFactory.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace EchoServer.Abstractions
-{
-    public interface ITcpListenerFactory
-    {
-        ITcpListenerWrapper Create(IPAddress address, int port);
-    }
-}
