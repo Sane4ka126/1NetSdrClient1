91c3410e4ec1a2d22783d38394cb7750620ff610
Sane4ka126 <evtikoleg@gmail.com>
2025-10-23 22:57:25 +0300

    Update NetworkWrappersTests.cs

--- NetSdrClientAppTests/NetworkWrappersTests.cs
+++ NetSdrClientAppTests/NetworkWrappersTests.cs
@@ -48,5 +48,72 @@ namespace NetSdrClientAppTests
             wrapperType.Should().NotBeNull();
             wrapperType.GetInterfaces().Should().Contain(typeof(INetworkStreamWrapper));
         }
+
+        [Test]
+        public void TcpListenerWrapper_ShouldStartAndStop()
+        {
+            // Arrange
+            var wrapper = new TcpListenerWrapper(IPAddress.Loopback, 0);
+
+            // Act
+            Action startAct = () => wrapper.Start();
+            Action stopAct = () => wrapper.Stop();
+
+            // Assert
+            startAct.Should().NotThrow();
+            stopAct.Should().NotThrow();
+            wrapper.Dispose();
+        }
+
+        [Test]
+        public void TcpClientWrapper_ShouldImplementInterface()
+        {
+            // Arrange & Act
+            var wrapperType = typeof(TcpClientWrapper);
+
+            // Assert
+            wrapperType.Should().Implement<ITcpClientWrapper>();
+        }
+
+        [Test]
+        public void TcpListenerFactory_ShouldCreateMultipleListeners()
+        {
+            // Arrange
+            var factory = new TcpListenerFactory();
+
+            // Act
+            using var listener1 = factory.Create(IPAddress.Loopback, 0);
+            using var listener2 = factory.Create(IPAddress.Loopback, 0);
+
+            // Assert
+            listener1.Should().NotBeNull();
+            listener2.Should().NotBeNull();
+            listener1.Should().NotBeSameAs(listener2);
+        }
+
+        [Test]
+        public void ConsoleLogger_ShouldLogErrorWithPrefix()
+        {
+            // Arrange
+            var logger = new ConsoleLogger();
+
+            // Act & Assert
+            Action act = () => logger.LogError("Critical error");
+            act.Should().NotThrow();
+        }
+
+        [Test]
+        public void TcpListenerWrapper_Dispose_ShouldNotThrow()
+        {
+            // Arrange
+            var wrapper = new TcpListenerWrapper(IPAddress.Loopback, 0);
+            wrapper.Start();
+
+            // Act
+            Action act = () => wrapper.Dispose();
+
+            // Assert
+            act.Should().NotThrow();
+        }
     }
 }
